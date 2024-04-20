4f20e73557686c67ecd2b9e2bac19bc3712000f0
IBO <reyza@ent-vision.com>
2024-04-20 09:14:24 +0700

    push all

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+// components
+import Hero from "@/components/Hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
+}
